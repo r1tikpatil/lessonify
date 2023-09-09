45120eeac82357eeda7c228d8c1b0262b91695eb
r1tikpatil <ritikpatil566@gmail.com>
2023-09-09 17:47:27 +0530

    build server

---
+++ server/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+const mongoose = require('mongoose');
+const cors = require('cors')
+
+
+const authRoutes = require('./routes/auth')
+const testRoutes = require('./routes/report')
+
+const app = express();
+
+app.use(cors())
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+//routes 
+
+app.use('/api',authRoutes)
+app.use('/api',testRoutes)
+
+
+const port = process.env.PORT || 8080;
+
+
+mongoose
+  .connect('mongodb+srv://lessonify:Ritik12345@cluster0.ctxug.mongodb.net/lessonify',{ useNewUrlParser: true ,useUnifiedTopology: true}
+  )
+  .then(result => {
+    app.listen(port);
+  })
+  .catch(err => console.log(err));
+
+ 
